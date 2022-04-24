a8780cfa32d79ae4d9d6e0fdf08f4490f273fce7
Allison Bohannon <akboh.github@gmail.com>
2022-04-23 17:30:07 -0700

    commit tree ade0e6c3bfd4124c3f992bfca7c0d2157a1f3e3c

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+main.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+
+newHeader.textContent = 'Allison is the champion'
+document.body.append(newHeader); 
+
+
